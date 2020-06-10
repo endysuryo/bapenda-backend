78b26b5fcc27fe59cde858a63f4b78432c7fcc08
endysuryo <endysuryalaksana@gmail.com>
2020-06-10 23:33:14 +0700

    Add first step counting cluster

--- src/customerBillboard/customerBillboard.service.ts
+++ src/customerBillboard/customerBillboard.service.ts
@@ -19,12 +19,53 @@ export class CustomerBillboardService extends TypeOrmCrudService<
       .getMany();
 
       const centroidData = dto;
+      const tempCluster = [];
+
+      for (const dataCluster of fetchAllData) {
+        const clusterValue1 = Math.sqrt(
+          Math.pow((dataCluster.subdistrict_weight - centroidData.cluster_1.subdistrict_weight), 2)
+          + Math.pow((dataCluster.billboard_total - centroidData.cluster_1.billboard_total), 2)
+          + Math.pow((dataCluster.billboard_weight - centroidData.cluster_1.billboard_weight), 2),
+        );
+        const clusterValue2 = Math.sqrt(
+          Math.pow((dataCluster.subdistrict_weight - centroidData.cluster_2.subdistrict_weight), 2)
+          + Math.pow((dataCluster.billboard_total - centroidData.cluster_2.billboard_total), 2)
+          + Math.pow((dataCluster.billboard_weight - centroidData.cluster_2.billboard_weight), 2),
+        );
+        const clusterValue3 = Math.sqrt(
+          Math.pow((dataCluster.subdistrict_weight - centroidData.cluster_3.subdistrict_weight), 2)
+          + Math.pow((dataCluster.billboard_total - centroidData.cluster_3.billboard_total), 2)
+          + Math.pow((dataCluster.billboard_weight - centroidData.cluster_3.billboard_weight), 2),
+        );
+
+        const compareValue = Math.min(clusterValue1, clusterValue2, clusterValue3);
+
+        let numberOfCluster = 0;
+
+        if (compareValue === clusterValue1) {
+          numberOfCluster = 1;
+        } else if (compareValue === clusterValue2) {
+          numberOfCluster = 2;
+        } else {
+          numberOfCluster = 3;
+        }
+
+        const fisrtStepCluster = {
+          ...dataCluster,
+          data_cluster1: clusterValue1,
+          data_cluster2: clusterValue2,
+          data_cluster3: clusterValue3,
+          minimum_cluster: numberOfCluster,
+        };
+
+        tempCluster.push(fisrtStepCluster);
+      }
+
+      // define new centroid
+
+      return tempCluster;
     } catch (err) {
       return Promise.reject(err);
     }
   }
-
-  // async kMeans(dtoKmeans) {
-  //   const kmeansValue =  Math.sqrt();
-  // }
 }
